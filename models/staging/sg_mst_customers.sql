b799a8edc63b2bf1a231fbc529b9e13a54698c3e
Kikgabong <gik.nuttha@gmail.com>
2026-03-15 23:02:51 +0700

    create stg_mst_customers staging model with data cleaning

---
+++ models/staging/sg_mst_customers.sql
@@ -0,0 +1,19 @@
+with source as (
+    select * 
+    from {{ source('raw_data', 'customers_master')}}
+),
+
+sg_mst_customers as (
+    select
+        trim(lower(customer_id)) as customer_id,
+        signup_date::date as signup_date,
+        trim(lower(customer_segment)) as customer_segment,
+        trim(lower("status")) as current_status,
+        trim(lower(referral_source)) as referral_source,
+        birth_year,
+        trim(lower(gender)) as gender,
+        trim(lower(preferred_device)) as preferred_device
+    from source
+)
+
+select * from sg_mst_customers
