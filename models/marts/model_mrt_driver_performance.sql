5d3f1f12b43f8bf20b975db9d8f394a5bc4ee3c9
Kikgabong <gik.nuttha@gmail.com>
2026-03-18 00:35:54 +0700

    model_mrt_driver_performance

--- models/marts/model_mrt_driver_performance.sql
+++ models/marts/model_mrt_driver_performance.sql
@@ -16,8 +16,8 @@ with driver_list as (
         count(case when is_late_delivery = 'TRUE' then 1 end) as total_late_deliveries,
         avg(csat_score) as avg_customer_csat
     from driver_order
-    group by 1
-)
+    group by driver_id
+    )
 
 select
     d.driver_id,
@@ -26,11 +26,12 @@ select
     d.driver_rating as lifetime_rating,
     m.total_assigned_tasks,
     m.total_completed_tasks,
+    round(m.total_completed_tasks * 100.0 / nullif(m.total_assigned_tasks,0), 2) as completion_rate_pct,
     round(m.avg_acceptance_time_min, 2) as avg_acceptance_time_min,
     round(m.avg_delivery_time_min, 2) as avg_delivery_time_min,
     m.total_late_deliveries,
-    round((m.total_late_deliveries * 100.0 / nullif(m.total_completed_tasks, 0)), 2) as late_delivery_rate_pct,
+    round(m.total_late_deliveries * 100.0 / nullif(m.total_completed_tasks, 0), 2) as late_delivery_rate_pct,
     round(m.avg_customer_csat, 2) as avg_recent_csat
 from driver_list d
-left join driver_perf m on d.driver_id = m.driver_id
-
+left join driver_perf m 
+    on d.driver_id = m.driver_id
