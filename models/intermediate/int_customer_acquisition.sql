b0220ca07f083ea9458ab55de07c02bcc757abed
Kikgabong <gik.nuttha@gmail.com>
2026-03-17 22:30:51 +0700

    git commit -m "update int_customer_acquisition model"

--- models/intermediate/int_customer_acquisition.sql
+++ models/intermediate/int_customer_acquisition.sql
@@ -1,41 +1,39 @@
-with campaign as (
-    select * from {{ref('sg_mst_campaign')}}
-),
-
-interactions as (
+with interactions as (
     select * from {{ ref('sg_log_camp_interac') }}
-),
+    ),
 
-orders as (
-select * from {{ref('sg_trn_order')}}
-),
+    orders as (
+    select * from {{ref('sg_trn_order')}}
+    ),
 
-acquisition as (
-    select
-        customer_id,
-        interaction_dt,
-        ad_cost as acquisition_cost,
-        platform,
-        order_id
-    from interactions
-    where TRUE
-        and is_new_customer = true
-        and event_type = 'conversion' 
-),
+    acquisition as (
+        select
+            campaign_id,
+            customer_id,
+            interaction_dt,
+            ad_cost as acquisition_cost,
+            platform,
+            order_id
+        from interactions
+        where TRUE
+            and is_new_customer = true
+            and event_type = 'conversion' 
+    ),
 
-customer_order as (
-    select
-        customer_id,
-        count(order_id) as total_orders,
-        sum(total_amount) as total_spend,
-        min(order_datetime) as first_order_dt,
-        max(order_datetime) as last_order_dt
-    from orders
-    where order_status = 'completed'
-    group by customer_id
-)
+    customer_order as (
+        select
+            customer_id,
+            count(order_id) as total_orders,
+            sum(total_amount) as total_spend,
+            min(order_datetime) as first_order_dt,
+            max(order_datetime) as last_order_dt
+        from orders
+        where order_status = 'completed'
+        group by customer_id
+    )
 
 select
+    a.campaign_id,
     a.customer_id,
     a.platform,
     a.order_id,
@@ -45,7 +43,6 @@ select
     --Repeat Behavior--
     c.total_orders,
     c.total_spend,
-    (c.total_spend / c.total_orders) as avg_order_value,
     c.last_order_dt,
     date_diff('day', c.first_order_dt, c.last_order_dt) as retention_days,
     a.acquisition_cost
