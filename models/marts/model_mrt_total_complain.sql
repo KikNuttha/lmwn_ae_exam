25beb81d52bf833949c9ea06c9858b75424ee382
Kikgabong <gik.nuttha@gmail.com>
2026-03-18 21:40:21 +0700

    update model_mrt_total_complain by include mm and yyyy

--- models/marts/model_mrt_total_complain.sql
+++ models/marts/model_mrt_total_complain.sql
@@ -4,6 +4,8 @@ with complaint_metrics as (
 
 select
     opened_date,
+    opened_date_mm,
+    opened_date_yyyy,
     issue_type,
     issue_sub_type,
     count(ticket_id) as total_tickets,
@@ -12,5 +14,5 @@ select
     sum(compensation_amount) as total_compensation_issued,
     round(sum(compensation_amount) / nullif(count(ticket_id), 0), 2) as avg_compensation_per_ticket
 from complaint_metrics
-group by 1, 2, 3
+group by 1, 2, 3, 4, 5
 order by cast(opened_date as date) desc, total_tickets desc
